702e976428805d1b725b7b2a49e15a39ba08ee64
shiqiang <qshi@suntekcorps.com>
2022-07-18 18:27:58 +0800

    调整配置

---
+++ demo/test.sh
@@ -0,0 +1,27 @@
+build       Build or rebuild services --建立或重建服务
+convert     Converts the compose file to platforms canonical format --将撰写文件转换为平台的规范格式
+cp          Copy files/folders between a service container and the local filesystem --在服务容器和本地文件系统之间复制文件/文件夹
+create      Creates containers for a service. --为服务创建容器。
+down        Stop and remove containers, networks  --停止并删除容器,网络
+events      Receive real time events from containers. --从容器中接收实时事件。
+exec        Execute a command in a running container. --在运行容器中执行命令。
+images      List images used by the created containers  --列出创建容器使用的图像
+kill        Force stop service containers.  --强制停止服务容器。
+logs        View output from containers --从容器中查看输出
+ls          List running compose projects --列表运行构成项目
+pause       Pause services  --暂停服务
+port        Print the public port for a port binding. --打印公共端口以绑定端口。
+ps          List containers --列表容器
+pull        Pull service images --拉服务图像
+push        Push service images --推送服务图像
+restart     Restart containers  --重新启动容器
+rm          Removes stopped service containers  --卸下停止服务容器
+run         Run a one-off command on a service. --在服务上运行一次性命令。
+start       Start services  --开始服务
+stop        Stop services --停止服务
+top         Display the running processes --显示运行过程
+unpause     Unpause services  --解开服务
+up          Create and start containers --创建和启动容器
+version     Show the Docker Compose version information --显示Docker撰写版本信息
+
+docker compose up
